80bac8f62c24e54f537f40d7b7d1c7a19cc07d19
Aishwarya Joy <113132012+iam-aishwaryajoy@users.noreply.github.com>
2024-09-29 19:51:43 +0000

    Model Prediction using Random forest and Linear Regression

--- source/libraries.py
+++ source/libraries.py
@@ -4,3 +4,8 @@ import pdb
 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
--- source/modelling.py
+++ source/modelling.py
@@ -9,6 +9,41 @@ class DeliverySystem:
     def data_making(self):
         transf = Datatransformation(self.train, self.test)
         self.train, self.test, self.org_train, self.org_test = transf.preprocess_the_model()
+        target_column = 'Time_taken(min)'  # Replace with your actual target column name
+
+        # For training data
+        X = self.train.drop(columns=[target_column])  # Drop the target column to get features
+        y = self.train[target_column]    
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)              # Get the target variable
+
+        print('Step 2: Modelling')
+        print(f"    X_train shape: {X_train.shape}")
+        print(f"    y_train shape: {y_train.shape}")
+        print(f"    X_test shape: {X_test.shape}")
+        print(f"    y_test shape: {y_test.shape if y_test is not None else 'N/A'}")
+
+        print('    Linear regression')
+        linear_regression = LinearRegression()
+        linear_regression.fit(X_train, y_train)
+        y_pred_LR = linear_regression.predict(X_test)
+
+        print('    Random Forest')
+        rf = RandomForestRegressor(n_estimators=100, random_state=42)
+        rf.fit(X_train, y_train)
+        y_pred_RF = rf.predict(X_test)
+
+
+        print('Step 3: Model Results:')
+        print('    Linear regression')
+        print('        Mean squared Error:', mean_squared_error(y_test, y_pred_LR))
+        print('        R2 Score:', r2_score(y_test, y_pred_LR))
+
+        print('    Random Forest')
+        print('        Mean squared Error:', mean_squared_error(y_test, y_pred_RF))
+        print('        R2 Score:', r2_score(y_test, y_pred_RF))
+
+        print('    Random forest is the better choice.')
+
 
      
             
